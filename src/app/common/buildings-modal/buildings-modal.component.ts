ccdd8315bc76f18895d0922700479a692b20ff82
MarcinTomkiewicz <marc.tomk@gmail.com>
2024-05-24 14:25:36 +0200

    Added corrected reaction for different requirement types possible.

--- src/app/common/buildings-modal/buildings-modal.component.ts
+++ src/app/common/buildings-modal/buildings-modal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { IBuilding } from '../../interfaces/definitions/i-building';
 import { CommonModule } from '@angular/common';
+import { IBuildingRequirement, IHeroLevelRequirement, IHeroStatRequirement, IRequirement } from '../../interfaces/definitions/i-requirements';
 
 @Component({
   selector: 'app-buildings-modal',
@@ -25,5 +26,18 @@ export class BuildingsModalComponent {
 
   cancel() {
     this.activeModal.dismiss('cancel');
+  }
+
+  getRequirementDescription(requirement: IRequirement): string {
+    switch (requirement.type) {
+      case 'building':
+        return `${requirement.buildingId} level ${requirement.level},`;
+      case 'heroStat':
+        return `${requirement.stat} ${requirement.value},`;
+      case 'heroLevel':
+        return `${requirement.type} ${requirement.value},`;
+      default:
+        return `Unknown requirement type`;
+    }
   }
 }
